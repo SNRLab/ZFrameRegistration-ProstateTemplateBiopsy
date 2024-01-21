225a7ce874457ba01c2ae9f21f440b774cd30c77
Franklin King <fking1@bwh.harvard.edu>
2024-01-21 02:45:16 -0500

    Added support for a 4th Z-frame (wide model)

--- ZFrameRegistration/ZFrameRegistration.cxx
+++ ZFrameRegistration/ZFrameRegistration.cxx
@@ -66,7 +66,7 @@ int main( int argc, char * argv[] )
     MatrixType imageToWorldTransform;
     imageToWorldTransform = lps2RasTransformMatrix * rtimgTransform;
     
-    // Convert image positiona and orientation to zf::Matrix4x4
+    // Convert image position and orientation to zf::Matrix4x4
     zf::Matrix4x4 imageTransform;
     imageTransform[0][0] = imageToWorldTransform[0][0];
     imageTransform[1][0] = imageToWorldTransform[1][0];
@@ -113,21 +113,139 @@ int main( int argc, char * argv[] )
     float Zorientation[4];
     
     // Call Z-frame registration
-    zf::Registration * registration;
-    registration = new zf::Registration();
+    // zf::Registration * registration;
+    // registration = new zf::Registration();
     
     int dim[3];
     dim[0] = dimensions[0];
     dim[1] = dimensions[1];
     dim[2] = dimensions[2];
+    int r = -1;
+    bool manualRegistration = false;
+
+    // std::cout << "zframeConfig: " << zframeConfig << std::endl;
+    // std::cout << "Frame topology: " << frameTopology << std::endl;
+    // std::cout << "zFrameFids: " << zFrameFids << std::endl;
+
+    // Convert frameTopology string back into an array of floats
+    float frameTopologyArr[6][3];
+    float x, y, z;
+    std::string substring, z_substr;
+    size_t pos, pos_start, pos_end = 0;
+
+    int numFids = 0;
+    if (zframeConfig == "z001") { numFids = 7; }
+    else if (zframeConfig == "z002" || zframeConfig == "z003") { numFids = 9; }
+    //float zFrameFidsArr[numFids][2];
+	auto zFrameFidsArr = new float[numFids][2];
+
+    for (int i = 0; i <= 5; ++i)
+    {
+        pos_start = frameTopology.find("["); 
+        pos_end = frameTopology.find("]"); 
+        substring = frameTopology.substr(pos_start + 1, pos_end);
+        frameTopology.erase(0, pos_end + 1); // Erase the first occurence from frameTopology string
+
+        // std::cout << "frameTopology: " << frameTopology << std::endl;
+        // std::cout << "substring: " << substring << std::endl;
+
+        // Separate the substring at the commas twice and use std::stof() to convert the string to float to find x, y, and z
+        pos = substring.find(",");
+        x = std::stof(substring.substr(0, pos));
+        substring.erase(0, pos + 1);
+
+        pos = substring.find(",");
+        y = std::stof(substring.substr(0, pos));
+        z = std::stof(substring.erase(0, pos + 2 ));
+
+        frameTopologyArr[i][0] = x;
+        frameTopologyArr[i][1] = y;
+        frameTopologyArr[i][2] = z;
+    }
+    // Final result is stored in frameTopologyArr[6][3]
+    // The first 3 rows contain the origin points in RAS coordinates of Side 1, Base, and Side 2, respectively. 
+    // The 4th, 5th, and 6th rows contain the diagonal vectors in RAS coordinates of Side 1, Base, and Side 2.
+
+    // If zFrameFidsString is not empty, then the user manually selected points to use in registration. 
+    // Convert zFrameFidsString back into an array of floats for use in the registration algorithm
+    if (zFrameFids.length() > 1)
+    {   
+        manualRegistration = true;
+        for (int i = 0; i < numFids; ++i)
+        {
+            pos_start = zFrameFids.find("["); 
+            pos_end = zFrameFids.find("]"); 
+            substring = zFrameFids.substr(pos_start + 1, pos_end);
+            zFrameFids.erase(0, pos_end + 1); // Erase the first occurence from frameTopology string
+
+            // Separate the substring at the commas twice and use std::stof() to convert the string to float to find x, y, and z
+            pos = substring.find(",");
+            x = std::stof(substring.substr(0, pos));
+            substring.erase(0, pos + 1);
+
+            pos = substring.find(",");
+            // y = std::stof(substring.substr(0, pos));
+            y = std::stof(substring.substr(0, pos));
+            // z = std::stof(substring.erase(0, pos + 2 ));
+
+            zFrameFidsArr[i][0] = x;
+            zFrameFidsArr[i][1] = y;
+            // zFrameFidsArr[i][2] = z;
+
+            std::cout << "zFrameFidsArr: " << zFrameFidsArr[i][0] << ", " << zFrameFidsArr[i][1] << std::endl;
+        }
+    }
+
+    // Toggle registration algorithm based on zframe configuration (currently, only z001 [7 fid], z002 [9 fid], z003 [9fid], and z004 [7fid])
+    if (zframeConfig == "z001" || zframeConfig == "z004")
+    {
+        // Zframe z001 is a 7-fiducial frame; run the 7-fiducial registration [Namespace zf_7fid]
+        // Zframe z004 is a 7-fiducial frame; run the 7-fiducial registration [Namespace zf_7fid]
+        zf_7fid::Registration * registration;
+        registration = new zf_7fid::Registration();
+        
+        registration->SetInputImage(image->GetBufferPointer(), dim, imageTransform);
+        registration->SetOrientationBase(ZquaternionBase);
+        registration->SetFrameTopology(frameTopologyArr);
+        if (manualRegistration) { registration->SetManualZFrameFiducials(zFrameFidsArr, manualRegistration); }
+        else { registration->SetAutomaticRegistration(manualRegistration); }
+        r = registration->Register(range, Zposition, Zorientation);
+        
+        delete registration;
+        cout << r << endl;
+    }
+    else if (zframeConfig == "z002" || zframeConfig == "z003")
+    {
+        // Zframe z002 is a 9-fiducial frame with the base Z on the top; run the 9-fiducial registration with baseLocation "top" [Namespace zf_9fid]
+        // Zframe z003 is a 9-fiducial frame with the base Z on the bottom; run the 9-fiducial registration with baseLocation "bottom" [Namespace zf_9fid]
+        zf_9fid::Registration * registration;
+        registration = new zf_9fid::Registration();
+
+        if (zframeConfig == "z002") { registration->SetBaseLocation("top"); }
+        else if (zframeConfig == "z003") { registration->SetBaseLocation("bottom"); }
+        registration->SetInputImage(image->GetBufferPointer(), dim, imageTransform);
+        registration->SetOrientationBase(ZquaternionBase);
+        registration->SetFrameTopology(frameTopologyArr);
+        if (manualRegistration) { registration->SetManualZFrameFiducials(zFrameFidsArr, manualRegistration); }
+        else { registration->SetAutomaticRegistration(manualRegistration); }
+        r = registration->Register(range, Zposition, Zorientation);
+        
+        delete registration;
+        cout << r << endl;
+    }
+    else
+    {
+        std::cout << "Invalid z-frame configuration. Cannot run registration algorithm." << std::endl;
+        return EXIT_FAILURE ;
+    }
     
-    registration->SetInputImage(image->GetBufferPointer(), dim, imageTransform);
-    registration->SetOrientationBase(ZquaternionBase);
-    int r = registration->Register(range, Zposition, Zorientation);
+    //registration->SetInputImage(image->GetBufferPointer(), dim, imageTransform);
+    //registration->SetOrientationBase(ZquaternionBase);
+    // int r = registration->Register(range, Zposition, Zorientation);
     
-    delete registration;
+    // delete registration;
     
-    cout << r << endl;
+    // cout << r << endl;
     
     if (r)
     {
